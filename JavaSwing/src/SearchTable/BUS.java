03ca29c4cae6f22a5691c94f06fdb658689b8368
HiamKaito <tangchichung1872014@gmail.com>
2021-01-09 18:52:15 +0700

    show data to form
    Connect to database, get it and show to Form

--- JavaSwing/src/SearchTable/BUS.java
+++ JavaSwing/src/SearchTable/BUS.java
@@ -18,12 +18,40 @@ public class BUS {
             weapon.display();
         }
     }
-    
+
     public void dealDamage() {
         for (Weapon weapon : list_WP) {
             System.out.println(
                     weapon.getStrName() + " Deal Damage: "
-                    +weapon.dealDamage());
+                    + weapon.dealDamage());
         }
+    }
+
+    public String[] getInforImage() {
+        String[] arrImage = new String[list_WP.size()];
+        int i = 0;
+
+        for (Weapon weapon : list_WP) {
+            arrImage[i] = weapon.getStrImage();
+            i++;
+        }
+
+        return arrImage;
+    }
+    
+    public Weapon[] getInfor() {
+        Weapon[] weapons = new Weapon[list_WP.size()];
+        int i = 0;
+
+        for (Weapon weapon : list_WP) {
+            weapons[i] = weapon;
+            i++;
+        }
+
+        return weapons;
+    }
+    
+    public int getSize() {
+        return list_WP.size();
     }
 }
